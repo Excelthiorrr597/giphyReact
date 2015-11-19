1905e3bf11bf1339d8956652b1a36d7df1da2e18
Jonathon Gregory <JGregory@Jonathons-MacBook-Pro.local>
2015-11-19 14:15:04 -0600

    better version using more recent version of react

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -13,22 +13,128 @@ require("isomorphic-fetch")
 // code line # to source code line #), in realtime via websockets
 if (module.hot) {
     module.hot.accept()
-    module.hot.dispose(() => {
-        app()
-    })
 }
 
 // import {React, Component, DOM, Resolver, resolve} from 'react-resolver'
-import DOM from 'react-dom'
+import ReactDOM from 'react-dom'
 import React, {Component} from 'react'
 
-// other stuff that we don't really use in our own code
-var Pace = require("../bower_components/PACE/pace.js")
+let $ = require('jquery')
 
-function app() {
-    // start app
-    // new Router()
-    DOM.render(<p>test</p>, document.querySelector('.container'))
+var ajaxParams = {
+    url: 'http://api.giphy.com/v1/gifs/search',
+    data: {
+        q: 'south park',
+        api_key: 'dc6zaTOxFJmzC'
+    }
 }
 
-app()
+var GifsView = React.createClass({
+
+    getInitialState: () => {
+        return {
+            focusId: (location.hash.slice(1) || null)
+        }
+    },
+
+    _getSingleGif: function(gif) {
+        return <SingleGif key={gif.id} focusId={this.state.focusId} walkieTalkie={this._walkieTalkie} gif={gif} />
+    },
+
+    _handleClick: function() {
+        if (this.state.focusId) {
+            location.hash=''
+            this.setState({
+                focusId:null
+            })
+        }
+    },
+
+    _walkieTalkie: function(gifId) {
+        this.setState({
+            focusId: gifId
+        })
+    },
+
+    render: function() {
+        var gifs = this.props.gifs
+        return (
+            <div id="gifsContainer" onClick={this._handleClick} >
+                {gifs.map(this._getSingleGif)}
+            </div>
+            )
+    }
+})
+
+var SingleGif = React.createClass({
+
+    _handleClick: function() {
+        var walkieTalkie = this.props.walkieTalkie,
+            gifId = this.props.gif.id
+        location.hash = gifId
+        walkieTalkie(gifId)
+    },
+
+    render: function() {
+        var gif = this.props.gif,
+            gifId = gif.id,
+            focusId = this.props.focusId,
+            imgUrl = gif.images.downsized.url,
+            styleObj = {
+                display:'none'
+            },
+            styleObj2 = {
+                position:'fixed',
+                left:'50%',
+                top:'50%',
+                transform:'translate(-50%,-50%)',
+                width:'50%'
+            }
+
+        if (focusId===gifId || location.hash.slice(1)===gifId) {
+            styleObj = {
+                position:'fixed',
+                left:'0',
+                top:'0',
+                right:'0',
+                bottom:'0',
+                backgroundColor:'rgba(0,0,0,0.8)',
+            }
+        }
+
+        console.log(location.hash)
+        return (
+            <div className="singleGif" onClick={this._handleClick}>
+                <div style={styleObj} >
+                    <img src={imgUrl} style={styleObj2} />
+                </div>
+                <img src={imgUrl} />
+                <GifDetails details={gif} />
+            </div>
+            )
+    }
+})
+
+var GifDetails = React.createClass({
+
+    _getDate: function() {
+        var d = new Date(this.props.details.import_datetime)
+        return d.toString().split(" ").slice(0,4).join(" ")
+    },
+
+    render: function() {
+        var rating = this.props.details.rating,
+            postedDate = this._getDate()
+
+        return (
+            <div className="details">
+                <p>Rating: {rating}</p>
+                <p>{postedDate}</p>
+            </div>
+            )
+    }
+})
+
+$.ajax(ajaxParams).then((response)=>{
+    ReactDOM.render(<GifsView gifs={response.data} />, document.querySelector('#container'))
+})
